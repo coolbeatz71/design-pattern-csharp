defe31f1a3e3c54dad47f258125ba871879f0338
coolbeatz71 <sigmacool@gmail.com>
2025-08-03 13:17:04 +0200

    feat: bad example of strategy pattern

---
+++ DesignPatterns/Behavioral/Strategy/BadExample/Compressor.cs
@@ -0,0 +1,11 @@
+namespace DesignPatterns.Behavioral.Strategy.BadExample;
+
+/// <summary>
+/// Enum representing the available video compression algorithms.
+/// </summary>
+public enum Compressor
+{
+    Mov,
+    Mp4,
+    WebM
+}
