e2afeda00ac6224a94ff362a21919f5a1c480b0d
coolbeatz71 <sigmacool@gmail.com>
2025-08-04 10:49:58 +0200

    doc: document the classes/methods

--- DesignPatterns/Behavioral/Command/RemoteControl/GoodExample/DimCommand.cs
+++ DesignPatterns/Behavioral/Command/RemoteControl/GoodExample/DimCommand.cs
@@ -1,7 +1,26 @@
 namespace DesignPatterns.Behavioral.Command.RemoteControl.GoodExample;
 
-public class DimCommand(Light light): ICommand
+/// <summary>
+/// Concrete command that dims the light.
+/// </summary>
+/// <param name="light">The receiver of the request.</param>
+/// <remarks>
+/// Encapsulates the action to dim the light and delegates it to the `Light` receiver.
+///
+/// <para><b>Command Role</b> Binds the receiver to the "Dim" action.</para>
+///
+/// <example>
+/// <code>
+/// var light = new Light();
+/// var dimCommand = new DimCommand(light);
+/// var remote = new RemoteControl(dimCommand);
+/// remote.PressButton(); // Output: "Light is dim"
+/// </code>
+/// </example>
+/// </remarks>
+public class DimCommand(Light light) : ICommand
 {
+    /// <inheritdoc />
     public void Execute()
     {
         light.Dim();
