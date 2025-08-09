2d5089e19c4e32ecefd85a759ab1e5250330d39a
coolbeatz71 <sigmacool@gmail.com>
2025-08-09 18:54:33 +0200

    doc: document classes and methods

--- DesignPatterns/Behavioral/Observer/BadExample/BarChart.cs
+++ DesignPatterns/Behavioral/Observer/BadExample/BarChart.cs
@@ -1,7 +1,29 @@
 namespace DesignPatterns.Behavioral.Observer.BadExample;
 
+/// <summary>
+/// Represents a <b>bar chart</b> UI component that visualizes a list of integer values.
+/// </summary>
+/// <remarks>
+/// <para>
+/// In this <i>bad example</i> of the Observer Pattern, the <see cref="BarChart"/> class
+/// is tightly coupled to the <see cref="DataSource"/> class, which directly calls 
+/// <see cref="Render"/> when data changes.
+/// </para>
+/// <para>
+/// This is problematic because:
+/// <list type="bullet">
+///   <item>It forces <see cref="DataSource"/> to know about the <see cref="BarChart"/> implementation details.</item>
+///   <item>Adding new dependent types requires modifying <see cref="DataSource"/>, breaking the Open/Closed Principle.</item>
+///   <item>It becomes messy and error-prone when the number of dependent views increases.</item>
+/// </list>
+/// </para>
+/// </remarks>
 public class BarChart
 {
+    /// <summary>
+    /// Renders the bar chart with the provided values.
+    /// </summary>
+    /// <param name="values">A collection of integer values to visualize.</param>
     public void Render(List<int> values)
     {
         Console.WriteLine("Rendering bar chart with new values");
--- DesignPatterns/Behavioral/Observer/BadExample/DataSource.cs
+++ DesignPatterns/Behavioral/Observer/BadExample/DataSource.cs
@@ -1,20 +1,48 @@
 namespace DesignPatterns.Behavioral.Observer.BadExample;
 
+/// <summary>
+/// Represents the data model in this bad Observer example.
+/// </summary>
+/// <remarks>
+/// <para>
+/// This class stores and manages a list of integer values, and it is responsible for
+/// notifying dependent objects whenever data changes.
+/// </para>
+/// <para>
+/// <b>Why this implementation is bad:</b>
+/// <list type="bullet">
+///   <item><b>Tight coupling:</b> The class explicitly knows and checks the types of its dependents (<see cref="BarChart"/>, <see cref="Sheet"/>).</item>
+///   <item><b>Violation of Open/Closed Principle:</b> Adding a new dependent type (e.g., <c>PieChart</c>) requires modifying this class.</item>
+///   <item><b>Poor scalability:</b> As the number of dependents grows, the <c>switch</c> statement becomes larger and harder to maintain.</item>
+/// </list>
+/// </para>
+/// <para>
+/// <b>Better Approach:</b> Use the <b>Observer Pattern</b> by introducing a common 
+/// <c>IObserver</c> interface that all dependents implement, allowing the subject 
+/// (<see cref="DataSource"/>) to notify them without knowing their concrete types.
+/// </para>
+/// </remarks>
 public class DataSource
 {
     private List<int> _values = [];
     private readonly List<object> _dependents = [];
-    
+
+    /// <summary>
+    /// Retrieves the current list of values stored in the data source.
+    /// </summary>
     public List<int> GetValues() => _values;
 
+    /// <summary>
+    /// Updates the list of values and refreshes all registered dependents.
+    /// </summary>
+    /// <param name="values">The new list of integer values.</param>
     public void SetValues(List<int> values)
     {
         _values = values;
-        
-        // We need to update our dependents object
+
+        // Notifying dependents (Bad: tightly coupled and type-dependent logic)
         foreach (var dependent in _dependents)
         {
-            // This is going to be VERY messy if we end up with lots of dependents!!
             switch (dependent)
             {
                 case Sheet sheet:
@@ -27,11 +55,19 @@ public class DataSource
         }
     }
 
+    /// <summary>
+    /// Registers a dependent object to be updated when the data changes.
+    /// </summary>
+    /// <param name="dependent">The dependent object (view or other observer).</param>
     public void AddDependent(object dependent)
     {
         _dependents.Add(dependent);
     }
 
+    /// <summary>
+    /// Removes a dependent object from the update list.
+    /// </summary>
+    /// <param name="dependent">The dependent object to remove.</param>
     public void RemoveDependent(object dependent)
     {
         _dependents.Remove(dependent);
